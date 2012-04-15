0781dcc5dbb6bd05f1c401c7b0fe2cefbb775841
root <root@kevin-narwahl.(none)>
2012-04-14 23:25:38 -0700

    stuff

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :price, :numericality => {
+    :greater_than_or_equal_to => 0.01,
+    :message => 'too low damnit'}
   validates :title, :uniqueness => true
   validates :image_url, :format => {
     :with     => %r{\.(gif|jpg|png)$}i,
