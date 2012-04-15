a2e5d8b9a108eb0d043aaf8a1ce6a08a2e2b4639
root <root@kevin-narwahl.(none)>
2012-04-14 22:51:02 -0700

    Validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,20 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+test "product price must be positive" do 
+product = Product.new(:title => "My Book Title",
+ :description => "yyy", 
+:image_url => "zzz.jpg") 
+product.price = -1 
+assert product.invalid? 
+assert_equal "must be greater than or equal to 0.01", 
+product.errors[:price].join('; ') 
+product.price = 0 
+assert product.invalid? 
+assert_equal "must be greater than or equal to 0.01", 
+product.errors[:price].join('; ') 
+product.price = 1 
+assert product.valid? 
+end 
+
 end
